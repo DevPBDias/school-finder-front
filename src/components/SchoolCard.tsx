07626d5d5e39301d04a1ea3d2f2bd972fe9ec1d4
DevPBDias <devpbdias@gmail.com>
2024-03-06 17:06:25 -0300

    fix: fixed styles to show slides

--- src/components/SchoolCard.tsx
+++ src/components/SchoolCard.tsx
@@ -1,23 +1,38 @@
 import { styled } from "styled-components"
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
 
-const CardContent = styled.div`
-    width: 28em;
-    height: 18em;
-    border: none;
-    border-radius: 8px;
-    position: relative;
-    margin: 1em auto;
+const CardContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  width: 100%;
+  padding: 1em;
+  gap: 1em;
+  
+  .div-slider {
+      width: 95%;
+      margin: auto;
+    }
+    
+    .link {
+        width: 15em;
+        height: 14em;
+        border: none;
+        border-radius: 8px;
+        position: relative;
 
+        
     .img-bg {
         position: absolute;
         z-index: 1;
         width: 100%;
         height: 100%;
-
+        padding: .3em;
 
         img {
-    border-radius: 8px;
-
+            border-radius: 8px;
             width: 100%;
             height: 100%;   
         }
@@ -26,29 +41,30 @@ const CardContent = styled.div`
     .text-content {
         position: absolute;
         z-index: 2;
+        top: .3em;
+        right: .3em;
         margin-left: 50%;
         width: 50%;
-        height: 100%;
+        height: 95%;
         background-color: #2B335B;
         opacity: 80%;
-    border-radius: 8px;
-    padding: 1em;
-    display: flex;
-    flex-flow: column nowrap;
-    gap: .2em;
-
+        border-radius: 8px;
+        padding: .5em;
+        display: flex;
+        flex-flow: column nowrap;
+        gap: .2em;
         
         h1 {
             color: #FABC2A;
-            font-size: 1.1rem;
+            font-size: .9rem;
             font-weight: 600;
-            margin-bottom: 2em;
+            margin-bottom: 1em;
         }
 
         h2 {
             color: #FABC2A;
-            font-size: 1rem;
-            margin-top: 2em;
+            font-size: .85rem;
+            margin-top: 1em;
         }
 
         p {
@@ -56,34 +72,50 @@ const CardContent = styled.div`
             font-size: .8rem;
         }
     }
+}
 `
-
 
 function SchoolCards({ data }: any) {
     const getSchools = data?.map((elem: any) => elem)
+    const settings = {
+        className: "center",
+        infinite: true,
+        centerPadding: "60px",
+        slidesToShow: 3,
+        swipeToSlide: true,
+        afterChange: function (index: any) {
+            console.log(
+                `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+            );
+        }
+    };
 
     return (
-        <>
-            {
-                getSchools?.map((elem: any, index: number) => (
-                    <CardContent key={index}>
-                        <div className="img-bg">
-                            <img src={elem.image} alt="school image" />
-                        </div>
-                        <div className="text-content">
-                            <h1>{elem.name}</h1>
-                            <p>{elem.city}</p>
-                            <p>{elem.neighborhood}</p>
-                            <h2>Ensinos:</h2>
-                            <p>Jardim de infância: {elem.educationType.kindergarten.vacancies} vagas</p>
-                            <p>Escola Fundamental I: {elem.educationType.primarySchool.vacancies} vagas</p>
-                            <p>Escola Fundamental II: {elem.educationType.secondarySchool.vacancies} vagas</p>
-                            <p>Ensino médio: {elem.educationType.highSchool.vacancies} vagas</p>
-                        </div>
-                    </CardContent>
-                ))
-            }
-        </>
+        <CardContainer>
+            <div className="div-slider">
+                <Slider {...settings}>
+                    {
+                        getSchools?.map((elem: any, index: number) => (
+                            <Link className="link" key={index} to={`/school/${elem._id}`}>
+                                <div className="img-bg">
+                                    <img src={elem.image} alt="school image" />
+                                </div>
+                                <div className="text-content">
+                                    <h1>{elem.name}</h1>
+                                    <p>{elem.city}</p>
+                                    <p>{elem.neighborhood}</p>
+                                    <h2>Ensinos:</h2>
+                                    <p>Jardim de infância: {elem.educationType.kindergarten.vacancies} vagas</p>
+                                    <p>Escola Fundamental I: {elem.educationType.primarySchool.vacancies} vagas</p>
+                                    <p>Escola Fundamental II: {elem.educationType.secondarySchool.vacancies} vagas</p>
+                                    <p>Ensino médio: {elem.educationType.highSchool.vacancies} vagas</p>
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </Slider>
+            </div>
+        </CardContainer>
     )
 }
 
--- src/components/SchoolOptions.tsx
+++ src/components/SchoolOptions.tsx
@@ -2,30 +2,25 @@ import { useContext } from 'react'
 import { SchoolContext } from '../context/SchoolContext'
 import SchoolCard from './SchoolCard'
 import { useNavigate } from 'react-router-dom'
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { styled } from "styled-components"
+
+const OptionsContainer = styled.section`
+    width: 70%;
+    height: 30em;
+    border: none;
+    border-radius: 8px;
+    margin: 1em auto;
+    background-color: #CEF9F2;
+
+`
 
 
 function SchoolOptions() {
     const { schools } = useContext(SchoolContext)
     const navigage = useNavigate()
 
-    const settings = {
-        className: "center",
-        infinite: true,
-        centerPadding: "60px",
-        slidesToShow: 3,
-        swipeToSlide: true,
-        afterChange: function (index: any) {
-            console.log(
-                `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
-            );
-        }
-    };
-
     return (
-        <section>
+        <OptionsContainer>
             <h1>Escolas perto de você</h1>
             <div>
                 <h4>{schools.total} escolas para decidir onde seu filho vai estudar</h4>
@@ -36,12 +31,8 @@ function SchoolOptions() {
                     Encontrar escola
                 </button>
             </div>
-            <Slider {...settings}>
-                <div>
-                    <SchoolCard data={schools.items} />
-                </div>
-            </Slider>
-        </section>
+            <SchoolCard data={schools.items} />
+        </OptionsContainer>
     )
 }
 
